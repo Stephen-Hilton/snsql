28cf2a0033062a8439911b7af23d0bab0e5b34fb
Stephen-Hilton <stephen.hilton@familyhilton.com>
2022-03-13 22:54:42 -0700

    test case enhancement

--- tests/test_cases.sql
+++ tests/test_cases.sql
@@ -1,34 +1,37 @@
 
 chart bar 'this is my title' 
- colA as x(label='revenue', format='$0.00M')
-,colB as y(label=null)
+ colA as x(label='Account Health')
+,colB as y(label='Revenue', axis='primary', format='$0.00M')
+,colC as y(label=null, axis='primary')
 ,100 as y(type=line, color='#ff0000')
 ,20 as x(type=line, color='orange')
-from "./some/file.csv"
-where colC = 'test'
-legend (null)
+from file('./some/file.csv')
+where (colC = 'test') --<--- need to do this, and comments
+body (width=12, height=6, backcolor='light grey', line='solid', linecolor=grey)
+title (fontsize=12, fontfamily='arial', x=50%, y=0%, align='center')
+legend (fontsize=8, x=110%, y=25%, align='right', backcolor='light grey')
 order by colB desc 
 ;
 
 chart line 'coa chart of unknown upper bound' 
  logdate    as x(label=null, format='yyyy-mm')
-,CPU_Idle   as y(label='Idle', color='grey')
-,CPU_OS     as y(label='OS', color='light blue')
-,CPU_IOWait as y(label='IOWait', color='yellow')
-,CPU_DBMS   as y(label='DBMS', color='blue')
+,CPU_Idle   as y(label='Idle', color='grey', width=0.75)
+,CPU_OS     as y(label='OS', color='light blue', width=1.0)
+,CPU_IOWait as y(label='IOWait', color='yellow', width=1.0)
+,CPU_DBMS   as y(label='DBMS', color='blue', width=1.25)
 ,100        as y(label='total', color='#ff0000')
 ,80         as y(label='warning line', color='orange', style='dash')
 from dataframe(df)
 body (style='coa', width=12, height=6)
 legend (x=120, y=80, border=(backcolor='grey', color='black', style='solid'))
+
 order by LogDate
 ;
 
 
-chart Bar 'Account Health by Region - Count' 
- body(style='bod', width=12, height=6)
+chart bar 'Account Health by Region - Count' 
 ,Region     as x(label=null)
-,count(*)   as y(stacked='Health'
+,ARR        as y(stacked='Account_Health'
 				 ,order=('green','yellow','red','black','unclassified') 
 				 ,color=('green','yellow','#ff0000','black','grey'))
 from file('somefile.csv')
